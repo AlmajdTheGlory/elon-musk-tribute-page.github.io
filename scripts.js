4561bbd0b0242df5c10f6347eca6d2bcc9bf142d
unknown <{ID}+{username}@users.noreply.github.com>
2022-06-09 21:53:41 +0300

    fourth commit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,33 @@
-//Fixing text size
+document.querySelector(".bullet-point-1").addEventListener("mouseover", function () {
+    document.querySelector(".title-1").style.color = "#064663"
+})
+document.querySelector(".bullet-point-1").addEventListener("mouseout", function () {
+    document.querySelector(".title-1").style.color = "#ead7af"
+})
 
-fitText(document.querySelector("h1"), 0.6);
-fitText(document.querySelector("p"), 1.6);
 
+document.querySelector(".bullet-point-2").addEventListener("mouseover", function () {
+    document.querySelector(".title-2").style.color = "#064663"
+})
+document.querySelector(".bullet-point-2").addEventListener("mouseout", function () {
+    document.querySelector(".title-2").style.color = "#ead7af"
+})
+
+
+document.querySelector(".bullet-point-3").addEventListener("mouseover", function () {
+    document.querySelector(".title-3").style.color = "#064663"
+})
+document.querySelector(".bullet-point-3").addEventListener("mouseout", function () {
+    document.querySelector(".title-3").style.color = "#ead7af"
+})
+
+
+document.querySelector(".bullet-point-4").addEventListener("mouseover", function () {
+    document.querySelector(".title-4").style.color = "#064663"
+})
+document.querySelector(".bullet-point-4").addEventListener("mouseout", function () {
+    document.querySelector(".title-4").style.color = "#ead7af"
+})
 
 
 
